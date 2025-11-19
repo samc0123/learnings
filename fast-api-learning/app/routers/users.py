7d95714f826b74b68b3a5790eb9a29118490561e
samc0123 <schernov1999@gmail.com>
2025-11-19 16:45:28 -0500

    Added JSON file db for DEV

--- fast-api-learning/app/routers/users.py
+++ fast-api-learning/app/routers/users.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter, Path, Body
 from typing import Annotated
 from schemas.users import UserIn, UserOut
+import json
+import os
+
 
 router = APIRouter()
 
-fake_database = {}
+fake_database_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'models', 'fake_database.json'))
+
+
 
 
 
@@ -15,7 +20,20 @@ async def create_user(user_info: UserIn):
     data = user_info.model_dump()
     email_address = data.pop("email_address")
 
-    fake_database[email_address] = data
+    try:
+        with open(fake_database_path,'r') as f:
+            fake_database: dict = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        fake_database = {}
+        
+    if email_address in fake_database:
+        fake_database[email_address].update(data)
+    else:
+        fake_database[email_address] = data
+    with open(fake_database_path,'w') as f:
+        json.dump(fake_database, f)
+    f.close()
+    
 
     return user_info
 
