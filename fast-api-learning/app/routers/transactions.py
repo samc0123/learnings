9a0e2bba592b209394f8047b7a03e883be7114cd
samc0123 <schernov1999@gmail.com>
2025-11-25 17:05:07 -0500

    Began transactions routers

---
+++ fast-api-learning/app/routers/transactions.py
@@ -0,0 +1,9 @@
+# All routes related to POS transactions 
+
+
+from fastapi import APIRouter
+
+router = APIRouter()
+
+#TODO: Implement Fake database and transactions routes to track POS purchases 
+fake_database_path = ""
